957b84a5eac521eaeffb3e01bcef138c85ebb1c4
Dylan Bargteil <dylan.bargteil@gmail.com>
2020-07-23 11:50:51 -0400

    add user data processing code

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
 import altair as alt
+import pandas as pd
+
+from lib.git import get_user_events
 
 app = Flask(__name__)
+
+def count_events(user_data):
+    events = pd.DataFrame([{
+        'type': event['type'],
+        'datetime': pd.Timestamp(event['created_at'])
+    } for event in user_data])
+    counts = events.groupby([events['datetime'].dt.date, 'type']).count() \
+                 .rename(columns={'datetime': 'count'}) \
+                 .reset_index().rename(columns={'datetime': 'date'})
+    counts['date'] = counts['date'].astype(str)
+    return counts
 
 @app.route('/')
 def index():
